a8b03e19b65581ceb8144adad3b102a329c73ed7
Merlin Dust <MLS1723978237@163.com>
2020-09-15 12:41:51 +0800

    init repo

---
+++ src/main/java/com/merlin/merlinsblog/MerlinsblogApplication.java
@@ -0,0 +1,13 @@
+package com.merlin.merlinsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerlinsblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerlinsblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merlin/merlinsblog/MerlinsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlin.merlinsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerlinsblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
